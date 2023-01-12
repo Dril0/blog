f75e95a5b0d1aa234e39d493ba307849fa844ab1
Jeremias <jeremias.acurso@hotmail.com>
2023-01-12 17:48:12 -0300

    ruteamos la nueva funcion de crear posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="home"),
     path(
         "post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"
     ),  # PK es la primary key, es el id que agrega django a la base de datos.
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
 ]
